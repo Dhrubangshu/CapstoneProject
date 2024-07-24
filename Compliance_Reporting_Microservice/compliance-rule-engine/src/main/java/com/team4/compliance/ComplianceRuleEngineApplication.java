c734e65ec73263d2e68fa7eae875decda037abfd
Dhrubangshu <dhrubangshu99@gmail.com>
2024-07-24 15:17:27 +0530

    updated CapstoneProject

--- Compliance_Reporting_Microservice/compliance-rule-engine/src/main/java/com/team4/compliance/ComplianceRuleEngineApplication.java
+++ Compliance_Reporting_Microservice/compliance-rule-engine/src/main/java/com/team4/compliance/ComplianceRuleEngineApplication.java
@@ -1,19 +1,26 @@
 package com.team4.compliance;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import com.team4.report.ReportService;
+
+import net.sf.jasperreports.engine.JRException;
 
 public class ComplianceRuleEngineApplication {
 
     public static void main(String[] args) {
-                ComplianceRule rule = new ComplianceRule("rule1", "Device ID must be device1", config -> config.getDeviceId().equals("device1"));
+                ComplianceRule rule = new ComplianceRule("rule1", "Device ID must be device1", 
+                		config -> config.getDeviceId().equals("device1"));
+                ComplianceRule rule2 = new ComplianceRule("rule2", "Settings must be {...}", 
+                		config -> config.getDeviceId().equals("{...}"));
+
+               
         
       
-        ComplianceService service = new ComplianceService(List.of(rule));
-
-        
-        Configuration config = new Configuration("device1", "{...}");
-
-       
+        ComplianceService service = new ComplianceService(List.of(rule,rule2)); 
+        Configuration config = new Configuration("device34", "xxyy");
         List<String> result = service.analyzeCompliance(config);
 
        
@@ -21,6 +28,22 @@ public class ComplianceRuleEngineApplication {
             System.out.println("All rules are compliant.");
         } else {
             System.out.println("Non-compliant rules: " + result);
+        
+        
+        ReportService reportService = new ReportService();
+        try {
+            byte[] pdfReport = reportService.generateReport(List.of(config));
+            savePDF(pdfReport, "compliance_report.pdf");
+            System.out.println("PDF report generated and saved as compliance_report.pdf");
+        } catch (JRException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+    private static void savePDF(byte[] pdfData, String fileName) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            fos.write(pdfData);
         }
     }
 }
